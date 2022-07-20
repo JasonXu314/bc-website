c7d4bb39acd1f1805ac4aeaf556371872d61e651
Jason Xu <Xinwen31415@gmail.com>
2022-07-20 17:16:05 -0500

    added statement for anniversary

--- pages/statements.tsx
+++ pages/statements.tsx
@@ -15,8 +15,16 @@ const Statements: NextPage = () => {
 				<div className={styles.statement}>
 					<h4 className={styles.date}>07/27/2021</h4>
 					<div className={styles.part}>
-						We regret to inform the public that [REDACTED], ex-CFO and ex-CAO has departed from our company within the past month. We have
-						enjoyed his presence and his valuable contributions to our group and we jointly wish him well in the future.
+						We regret to inform the public that [REDACTED], ex-CFO and ex-CAO has departed from our company within the past month. We have enjoyed
+						his presence and his valuable contributions to our group and we jointly wish him well in the future.
+					</div>
+					<div className={styles.signature}>- The Based Capital Team</div>
+				</div>
+				<div className={styles.statement}>
+					<h4 className={styles.date}>07/20/2022</h4>
+					<div className={styles.part}>
+						This July 4th, we celebrated the successful conclusion of our first year of operations. We hope to continue our growth into this coming
+						year and continue to eagerly strive for excellence in our operations!
 					</div>
 					<div className={styles.signature}>- The Based Capital Team</div>
 				</div>
