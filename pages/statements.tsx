db0f03b4cb220dd5bb6d1ba845bc774d76eaa064
Jason Xu <Xinwen31415@gmail.com>
2023-04-26 10:47:58 -0500

    fixed announcement positioning and date

--- pages/statements.tsx
+++ pages/statements.tsx
@@ -13,6 +13,16 @@ const Statements: NextPage = () => {
 			<Navbar />
 			<div className={styles.content}>
 				<h1 className={styles.title}>Company Statements</h1>
+				<Statement date="04/24/2023" branch="Capital">
+					Yesterday was the 2nd anniversary of the conceptual foundation of our company. The name &ldquo;Based Capital&rdquo; didn&apos;t arise until
+					our establishment had officially unionized. Yet, our team all shared the same aspirations in wanting to seize the opportunities offered by
+					mining cryptocurrencies. Since our initial congregations and investments, Based Capital has become an international investment vehicle.
+					<br />
+					We, Based Capital, would like to direct a great thank you to our founder, <strong>Jacob Stolker</strong>, and all those who have been
+					involved with our institution. Our entire group&apos;s teamwork and collaboration have allowed our organization to achieve many business
+					accolades over these past two years. Subsequently, our mindset pushes us forward as we recognize that Based Capital&apos;s journey has just
+					begun. Here is to another year of success -- may this year be the most triumphant of them all!
+				</Statement>
 				<Statement date="09/19/2022" branch="Enterprise">
 					After much hard work, we are excited to announce the beginning of our first joint venture! Representatives of Based Enterprise have
 					negotiated an agreement with Rabbi Zevi Wineberg Shlit&quot;a to fund the printing of his new book, &ldquo;Baal Shem Tov&rdquo;. We wish
@@ -30,16 +40,6 @@ const Statements: NextPage = () => {
 				<Statement date="07/27/2021" branch="Capital">
 					We regret to inform the public that our ex-CFO and ex-CAO has departed from our company within the past month. His role will be taken over
 					by Matthew Levy. We have enjoyed his presence and his valuable contributions to our group and we jointly wish him well in the future.
-				</Statement>
-				<Statement date="04/24/2021" branch="Capital">
-					Yesterday was the 2nd anniversary of the conceptual foundation of our company. The name &ldquo;Based Capital&rdquo; didn&apos;t arise until
-					our establishment had officially unionized. Yet, our team all shared the same aspirations in wanting to seize the opportunities offered by
-					mining cryptocurrencies. Since our initial congregations and investments, Based Capital has become an international investment vehicle.
-					<br />
-					We, Based Capital, would like to direct a great thank you to our founder, <strong>Jacob Stolker</strong>, and all those who have been
-					involved with our institution. Our entire group&apos;s teamwork and collaboration have allowed our organization to achieve many business
-					accolades over these past two years. Subsequently, our mindset pushes us forward as we recognize that Based Capital&apos;s journey has just
-					begun. Here is to another year of success -- may this year be the most triumphant of them all!
 				</Statement>
 			</div>
 		</div>
