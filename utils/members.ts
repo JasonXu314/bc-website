92407615563e3ced8253b145599f8768a0d0dbe7
Jason Xu <Xinwen31415@gmail.com>
2023-07-13 10:08:13 -0500

    changed matthew rank

--- utils/members.ts
+++ utils/members.ts
@@ -33,7 +33,8 @@ export default [
 		shortName: 'matthew',
 		position: 'Chairman of the Board, CFO, International Business Relations Coordinator for the Middle East and Europe',
 		shortDescription: 'Dedicated to Torah Judaism and religious causes and student of history and philosophy, proud to serve in the IDF.',
-		description: `Among his roles at Based Capital, LLC are his assigned positions of Chairman of the Board of Directors and Chief Financial Officer of the company. He handles the legal and accounting necessities of the company, manages the group's finances, and coordinates international business relations in the Middle East and Europe. He is also a Private in the Israeli Defense Forces, where he serves in the Infantry Corps.`,
+		description: `Among his roles at Based Capital, LLC are his assigned positions of Chairman of the Board of Directors and Chief Financial Officer of the company. He handles the legal and accounting necessities of the company, manages the group's finances, and coordinates international business relations in the Middle East and Europe. He is also a Corporal in the Israeli Defense Forces, where he serves in the Infantry Corps.`,
 		linkedIn: 'https://www.linkedin.com/in/matthew-l-630925218/'
 	}
 ] as Member[];
+
