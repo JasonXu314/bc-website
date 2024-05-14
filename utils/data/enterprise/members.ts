09abfc3ad3a8b5ccd995eaa55ee9e1dfa52ea4d4
Jason Xu <Xinwen31415@gmail.com>
2024-05-14 15:24:20 -0500

    added more members

--- utils/data/enterprise/members.ts
+++ utils/data/enterprise/members.ts
@@ -12,7 +12,7 @@ export default [
 		shortName: 'alex',
 		position: 'Social Media Manager, Assistant Accountant',
 		shortDescription:
-			'Alexander Levy is the Social Media Manager and Assistant-Accountant for Based Capital. His interests include volunteerism, film, running, and medicine.',
+			'Alexander Levy is the Social Media Manager and Assistant-Accountant for Based Capital. His interests include volunteering, film, running, and medicine.',
 		description: `Alexander Levy is the Social Media Manager and Assistant-Accountant for Based Capital. His interests include volunteering, film, running, and medicine. He is currently studying Neuroscience on the Pre-Med track at Boston University.
 
 			His contributions include creating and managing all social media profiles and organization of financial analytics.`
@@ -42,6 +42,27 @@ export default [
 		- Host of Soul-Talk on a Jewish Internet Radio Station.
 		- Hosted a TV series on his book “Kabbalah Love.”
 		- Chabad Rabbi/Activist
+		`
+	},
+	{
+		name: 'James M. Seaman',
+		shortName: 'james',
+		position: 'Venture Partner',
+		shortDescription:
+			'James Seaman is an accomplished attorney with years of experience in business law, mergers and acquisitions, and commercial lending, with a focus on international business operations.',
+		description: `
+		James (Jim) Seaman concentrates his practice in general corporate law with an emphasis on sophisticated transactional work. He has more than 30 years of experience handling a variety of domestic and international business transactions, including buying and selling businesses and real estate, real estate development, mergers, recapitalizations and other forms of corporate restructurings. His clients range from the inventor or entrepreneur seeking advice on how to capitalize on a promising idea to well established family, investor and management-owned businesses needing advice on the legal aspects of their operations and a person who can successfully negotiate and close their deals.
+
+		Jim's international business practice has included working with clients to acquire companies overseas and establish distribution and agency relationships, preparing and negotiating license and joint venture agreements, assisting with establishing companies overseas and advising clients regarding U.S. law compliance matters as they pertain to international business transactions.
+		`
+	},
+	{
+		name: 'Solomon (Shlomo) Knopf',
+		shortName: 'solomon',
+		position: 'Venture Partner',
+		shortDescription: 'Solomon Knopf brings his 36 years of experience in real estate and his experience of living and working in Israel.',
+		description: `
+		Solomon Knopf has a rich background in real estate in the United States, having brokered real estate in New York City as a partner for PAL Royalty and founded his own brokerage firm, First Commonwealth Investment Company, dealing in the management and improvement of properties for various purposes, including corporate office buildings. He has previously worked in Israel, and will be using his vast experience to guide the joint venture.
 		`
 	}
 ] as Member[];
